4383ac5ae18e6ee88f3f3277af9d4d7767181000
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-25 00:36:35 -0300

    Funções de limiarização

---
+++ limiarizacao/limiar.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "ops.c"
+
+
+typedef struct data {
+    size_t tam;
+    double param[4];
+} data_t;
+
+
+
+typedef double limiar_fn(const double vizinhanca[], size_t tam, data_t data);
+
+static inline attribute(pure, nonnull)
+double limiariza(const double *restrict buffer, size_t buflen, data_t data, limiar_fn *limiar) {
+    size_t centro = (data.tam / 2) * (data.tam + 1);
+    double pixel = buffer[centro];
+
+    if (pixel > limiar(buffer, buflen, data)) {
+        return 1.0;
+    } else {
+        return 0.0;
+    }
+}
+
+#define LIMIARIZA(METODO) \
+    extern attribute(pure, nonnull) \
+    int limiariza_##METODO(const double *restrict buffer, intptr_t buflen, double *restrict retval, const data_t *restrict data) {\
+        *retval = limiariza(buffer, (size_t) buflen, *data, limiar_##METODO); \
+        return 1; \
+    }
+
+
+#define UNUSED attribute(unused)
+
+static inline attribute(const)
+double limiar_global(UNUSED const double buf[], UNUSED size_t len, data_t data) {
+    return data.param[0];
+}
+LIMIARIZA(global)
+
+static inline attribute(pure, nonnull)
+double limiar_bernsen(const double vizinhanca[], size_t tam, UNUSED data_t data) {
+    struct minmax z = minmax(vizinhanca, tam);
+    return (z.min + z.max) / 2;
+}
+LIMIARIZA(bernsen)
---
+++ limiarizacao/ops.c
@@ -0,0 +1,75 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+
+#define attribute(...) __attribute__((__VA_ARGS__))
+
+
+struct minmax {
+    double min, max;
+};
+
+
+static attribute(pure, nonnull)
+struct minmax minmax(const double array[], size_t len) {
+    double min, max;
+    size_t start;
+
+    if (len % 2 == 1) {
+        min = max = array[0];
+        start = 1;
+    } else if (array[0] < array[1]) {
+        min = array[0];
+        max = array[1];
+        start = 2;
+    } else {
+        min = array[1];
+        max = array[0];
+        start = 2;
+    }
+
+    for (size_t i = start; i < len; i += 2) {
+        double menor = array[i];
+        double maior = array[i+1];
+
+        if (maior < menor) {
+            menor = maior;
+            maior = array[i];
+        }
+
+        if (menor < min) min = menor;
+        if (maior > max) max = maior;
+    }
+
+    return (struct minmax) {.min = min, .max = max};
+}
+
+static attribute(pure, nonnull)
+double mean(const double array[], size_t len) {
+    double sum = 0;
+    for (size_t i = 0; i < len; i++) {
+        sum += array[i];
+    }
+    return sum / len;
+}
+
+struct avgstd {
+    double avg, std;
+};
+
+static attribute(pure, nonnull)
+struct avgstd avgstd(const double array[], size_t len) {
+    double sum = 0, sumsq = 0;
+    for (size_t i = 0; i < len; i++) {
+        double val = array[i];
+        sum += val;
+        sumsq += val * val;
+    }
+
+    double avg = sum / len;
+    double avgsq = sumsq / len;
+    double std = sqrt(avgsq - avg * avg);
+
+    return (struct avgstd) {.avg = avg, .std = std};
+}
