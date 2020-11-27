b629e25ee2cd90620c27750ee42266c15481665c
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-27 15:21:07 -0300

    Argmin e argmax

--- limiarizacao/metodos/ops.c
+++ limiarizacao/metodos/ops.c
@@ -6,43 +6,62 @@
 #define attribute(...) __attribute__((__VA_ARGS__))
 
 
+struct argminmax {
+    size_t min, max;
+};
+
+static attribute(pure, nonnull)
+struct argminmax argminmax(const double array[], size_t len) {
+    double min, max;
+    size_t idmin, idmax;
+    size_t start;
+
+    if (len % 2 == 1) {
+        idmin = idmax = 0;
+        start = 1;
+    } else if (array[0] < array[1]) {
+        idmin = 0;
+        idmax = 1;
+        start = 2;
+    } else {
+        idmin = 1;
+        idmax = 0;
+        start = 2;
+    }
+
+    min = array[idmin];
+    max = array[idmax];
+    for (size_t i = start; i < len; i += 2) {
+        size_t menor = i;
+        size_t maior = i+1;
+
+        if (array[maior] < array[menor]) {
+            menor = i+1;
+            maior = i;
+        }
+
+        if (array[menor] < min) {
+            min = array[menor];
+            idmin = menor;
+        }
+        if (maior > max) {
+            max = array[maior];
+            idmax = maior;
+        }
+    }
+
+    return (struct argminmax) {.min = idmin, .max = idmax};
+}
+
 struct minmax {
     double min, max;
 };
 
-
 static attribute(pure, nonnull)
 struct minmax minmax(const double array[], size_t len) {
-    double min, max;
-    size_t start;
+    struct argminmax id = argminmax(array, len);
 
-    if (len % 2 == 1) {
-        min = max = array[0];
-        start = 1;
-    } else if (array[0] < array[1]) {
-        min = array[0];
-        max = array[1];
-        start = 2;
-    } else {
-        min = array[1];
-        max = array[0];
-        start = 2;
-    }
-
-    for (size_t i = start; i < len; i += 2) {
-        double menor = array[i];
-        double maior = array[i+1];
-
-        if (maior < menor) {
-            menor = maior;
-            maior = array[i];
-        }
-
-        if (menor < min) min = menor;
-        if (maior > max) max = maior;
-    }
-
-    return (struct minmax) {.min = min, .max = max};
+    return (struct minmax) {.min = array[id.min], .max = array[id.max]};
 }
 
 static attribute(pure, nonnull)
