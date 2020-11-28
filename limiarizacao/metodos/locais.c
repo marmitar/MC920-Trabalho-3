c579241215e5443b8426f59b8e18b706d87525da
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-27 23:25:10 -0300

    Documentação das funções de C

--- limiarizacao/metodos/locais.c
+++ limiarizacao/metodos/locais.c
@@ -1,23 +1,30 @@
+/**
+ * Funções de limiarização em uma vizinhança.
+ */
 #include <stdint.h>
 #include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include <math.h>
 #include "ops.c"
 
-
+// Parâmetros da limiarização.
 typedef struct data {
     double param[4];
 } data_t;
 
 
-
+// Tipo de função que calcula o limiar de uma vizinhança.
 typedef double limiar_fn(const double vizinhanca[], size_t tam, data_t data);
 
 static inline attribute(pure, nonnull)
+/**
+ * Encontra o limiar da vizinhança e aplica de acordo
+ * com o pixel central.
+ */
 double limiariza(const double *restrict buffer, size_t buflen, data_t data, limiar_fn *limiar) {
+    // pixel central
     double pixel = buffer[buflen / 2];
 
+    // aplica de acordo com uma função de limiar da região.
     if (pixel > limiar(buffer, buflen, data)) {
         return 255.0;
     } else {
@@ -25,6 +32,8 @@ double limiariza(const double *restrict buffer, size_t buflen, data_t data, limi
     }
 }
 
+// Macro que gera a função de limiarização a ser exportada, com
+// as entradas e saídas necessárias.
 #define LIMIARIZA(METODO) \
     extern attribute(pure, nonnull) \
     int limiariza_##METODO(const double *restrict buffer, intptr_t buflen, double *restrict retval, const data_t *restrict data) {\
@@ -33,9 +42,13 @@ double limiariza(const double *restrict buffer, size_t buflen, data_t data, limi
     }
 
 
+// Marcador de parâmetro não utilizado na função.
 #define UNUSED attribute(unused)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método de Bernsen.
+ */
 double limiar_bernsen(const double vizinhanca[], size_t tam, UNUSED data_t data) {
     struct minmax z = minmax(vizinhanca, tam);
 
@@ -44,6 +57,9 @@ double limiar_bernsen(const double vizinhanca[], size_t tam, UNUSED data_t data)
 LIMIARIZA(bernsen)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método de Niblack.
+ */
 double limiar_niblack(const double vizinhanca[], size_t tam, data_t data) {
     struct avgstd xy = avgstd(vizinhanca, tam);
     double k = data.param[0];
@@ -53,6 +69,9 @@ double limiar_niblack(const double vizinhanca[], size_t tam, data_t data) {
 LIMIARIZA(niblack)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método de Sauvola e Pietaksinen.
+ */
 double limiar_sauvola(const double vizinhanca[], size_t tam, data_t data) {
     struct avgstd xy = avgstd(vizinhanca, tam);
     double k = data.param[0], R = data.param[1];
@@ -62,6 +81,9 @@ double limiar_sauvola(const double vizinhanca[], size_t tam, data_t data) {
 LIMIARIZA(sauvola)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método de Phansalskar, More e Sabale.
+ */
 double limiar_phansalskar(const double vizinhanca[], size_t tam, data_t data) {
     struct avgstd xy = avgstd(vizinhanca, tam);
     double k = data.param[0], R = data.param[1];
@@ -72,35 +94,46 @@ double limiar_phansalskar(const double vizinhanca[], size_t tam, data_t data) {
 LIMIARIZA(phansalskar)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método do Contraste.
+ */
 double limiar_contraste(const double vizinhanca[], size_t tam) {
     double pixel = vizinhanca[tam / 2];
 
     struct minmax z = minmax(vizinhanca, tam);
+    // proximidade do mínimo da vizinhança
     size_t dist_min = pixel - z.min;
+    // proximidade do máximo
     size_t dist_max = z.max - pixel;
 
+    // retorna o pixel, em vez do limite
     if (dist_min < dist_max) {
         return 255.0;
     } else {
         return 0.0;
     }
 }
-extern attribute(pure, nonnull) \
+extern attribute(pure, nonnull)
+// Função especial de limiarização do contraste.
 int limiariza_contraste(const double *restrict buffer, intptr_t buflen, double *restrict retval, UNUSED const data_t *_data) {
     *retval = limiar_contraste(buffer, (size_t) buflen);
     return 1;
 }
 
 static inline attribute(pure, nonnull)
+/**
+ * Método da Média.
+ */
 double limiar_media(const double vizinhanca[], size_t tam, UNUSED data_t data) {
     return mean(vizinhanca, tam);
 }
 LIMIARIZA(media)
 
 static inline attribute(pure, nonnull)
+/**
+ * Método da Mediana.
+ */
 double limiar_mediana(const double vizinhanca[], size_t tam, UNUSED data_t data) {
-    double *tmp = alloca(tam * sizeof(double));
-    memcpy(tmp, vizinhanca, tam * sizeof(double));
-    return median(tmp, tam);
+    return median(vizinhanca, tam);
 }
 LIMIARIZA(mediana)
--- limiarizacao/metodos/ops.c
+++ limiarizacao/metodos/ops.c
@@ -1,21 +1,32 @@
+/**
+ * Operações auxiliares para as funções de limiarização.
+ */
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
+// atributos do GCC
+#define attribute(...) \
+    __attribute__((__VA_ARGS__))
 
-#define attribute(...) __attribute__((__VA_ARGS__))
 
-
+// Saída com o mínimo e o máximo de um vetor.
 struct minmax {
     double min, max;
 };
 
 static attribute(pure, nonnull)
+/**
+ * Encontra o mínimo e o máximo de um vetor.
+ */
 struct minmax minmax(const double array[], size_t len) {
     double min, max;
     size_t start;
 
+    // seleciona o mín e máx dos iniciais
     if (len % 2 == 1) {
         min = max = array[0];
         start = 1;
@@ -28,8 +39,9 @@ struct minmax minmax(const double array[], size_t len) {
         max = array[0];
         start = 2;
     }
-
+    // para que reste um número para de elementos
     for (size_t i = start; i < len; i += 2) {
+        // que são analisados par a par
         double menor = array[i];
         double maior = array[i+1];
 
@@ -41,11 +53,14 @@ struct minmax minmax(const double array[], size_t len) {
         if (menor < min) min = menor;
         if (maior > max) max = maior;
     }
-
+    // retorno na struct specífica
     return (struct minmax) {.min = min, .max = max};
 }
 
 static attribute(pure, nonnull)
+/**
+ * Cálculo da média do vetor.
+ */
 double mean(const double array[], size_t len) {
     double sum = 0;
     for (size_t i = 0; i < len; i++) {
@@ -54,12 +69,17 @@ double mean(const double array[], size_t len) {
     return sum / len;
 }
 
+// Saída com a média e desv.pad. do vetor.
 struct avgstd {
     double mu, sigma;
 };
 
 static attribute(pure, nonnull)
+/**
+ * Encontra média e desvio padrão simultaneamente.
+ */
 struct avgstd avgstd(const double array[], size_t len) {
+    // soma e soma dos quadrados
     double sum = 0, sumsq = 0;
     for (size_t i = 0; i < len; i++) {
         double val = array[i];
@@ -67,22 +87,32 @@ struct avgstd avgstd(const double array[], size_t len) {
         sumsq += val * val;
     }
 
+    // média
     double avg = sum / len;
+    // desvio padrão
     double avgsq = sumsq / len;
     double std = sqrt(avgsq - avg * avg);
-
+    // retorno na struct specífica
     return (struct avgstd) {.mu = avg, .sigma = std};
 }
 
 static attribute(nonnull)
+/**
+ * Particionamento para encontrar
+ */
 size_t partition(double array[], size_t len) {
-    double pivot = array[0];
+    // pivô como elemento na posição média
+    double pivot = array[len / 2];
+    array[len / 2] = array[0];
 
     size_t lo = 1, hi = len-1;
     while (lo <= hi) {
+        // move os elementos menores para
+        // as posições iniciais
         if (array[lo] <= pivot) {
             array[lo-1] = array[lo];
             lo++;
+        // e os maiores para o final
         } else {
             double tmp = array[hi];
             array[hi] = array[lo];
@@ -90,12 +120,15 @@ size_t partition(double array[], size_t len) {
             hi--;
         }
     }
-
+    // guarda o pivo na sua posição correta
     array[hi] = pivot;
     return hi;
 }
 
 static attribute(pure, nonnull)
+/**
+ * Encontra o mínimo do vetor.
+ */
 double dmin(const double array[], size_t len) {
     double min = array[0];
     for (size_t i = 1; i < len; i++) {
@@ -107,6 +140,9 @@ double dmin(const double array[], size_t len) {
 }
 
 static attribute(pure, nonnull)
+/**
+ * Encontra o máximo do vetor.
+ */
 double dmax(const double array[], size_t len) {
     double max = array[0];
     for (size_t i = 1; i < len; i++) {
@@ -118,30 +154,49 @@ double dmax(const double array[], size_t len) {
 }
 
 static attribute(nonnull)
-double median(double array[], size_t len) {
+/**
+ * Encontra a mediana de um vetor positivo.
+ */
+double median(const double input[], size_t len) {
+    // copia o vetor para o heap
+    double *array = alloca(len * sizeof(double));
+    memcpy(array, input, len * sizeof(double));
+
+    // posição das medianas
     size_t k1 = (len - 1) / 2;
     size_t k2 = len / 2;
+    // valores das medianas (negativo deve ser inválido)
     double m1 = -1.0, m2 = -1.0;
 
     size_t lo = 0;
+    // a mediana real será positiva
     while (m1 < 0.0) {
+        // posição resultado do particionamento
         size_t q = lo + partition(array + lo, len - lo);
+        // se menor, busca nos maiores
         if (q < k1) {
             lo = q + 1;
+        // se maior, busca nos menores
         } else if (q > k2) {
             len = q;
+        // então é alguma das medianas
         } else {
+            // se for a menor, marca ela
             if (q == k1) {
                 m1 = array[q];
+            // senão busca o maior dos menores
             } else {
                 m1 = dmax(array + lo, q - lo);
             }
+            // se for a maior, marca ela
             if (q == k2) {
                 m2 = array[q];
+            // senão busca a menor das maiores
             } else {
                 m2 = dmin(array + q + 1, len - q - 1);
             }
         }
     }
+    // média das medianas
     return (m1 + m2) / 2;
 }
