80276608cfc292e1074bb847ed987173f8ba2697
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-01 19:46:33 -0300

    Histogramas

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ mkdir -p resultados/hist
 for img in $(ls imagens)
 do
     nome="${img%.*}"
-    echo $img $nome
     convert imagens/$img resultados/base/$nome.png
     python3 histograma.py imagens/$img resultados/hist/$nome.pgf
 done
