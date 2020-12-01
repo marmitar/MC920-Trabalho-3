57f7f7944b64520a98b898010531465e66a6c99f
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-01 19:42:14 -0300

    Geração de histogramas

--- histograma.py
+++ histograma.py
@@ -1,6 +1,7 @@
 import sys, cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 import seaborn as sns
 sns.set_theme(context='paper', style='darkgrid', palette='muted', rc={
@@ -41,6 +42,7 @@ img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 sns.histplot(x=img.flat, bins=np.arange(256), fill=False, color='k')
 plt.xlabel('Valor do Pixel')
 plt.ylabel('Número de Pixels')
+plt.gca().xaxis.set_major_locator(MultipleLocator(64))
 
 if output is None:
     plt.show()
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mkdir -p resultados/base
+mkdir -p resultados/hist
 for img in $(ls imagens)
 do
     nome="${img%.*}"
     echo $img $nome
     convert imagens/$img resultados/base/$nome.png
+    python3 histograma.py imagens/$img resultados/hist/$nome.pgf
 done
