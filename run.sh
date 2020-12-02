26fa30e3027f61545d8a7fa7de29786bff907e74
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-02 01:58:08 -0300

    Método de Sauvola e Pietaksinen

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ function run {
     argv=($@)
     python3 limiarizacao -o resultados/$1/$3.png \
         imagens/$2.pgm $1 ${argv[@]:3}
-    echo $1 $3 $(python3 porcentagem.py resultados/$1/$3.png)
+    echo $1 $3 ${argv[@]:3} $(python3 porcentagem.py resultados/$1/$3.png)
 }
 
 mkdir -p resultados/global
@@ -31,7 +31,13 @@ mkdir -p resultados/bernsen
 # run bernsen sonnet sonnet -r 20
 
 mkdir -p resultados/niblack
-run niblack sonnet sonnet50 -r 50 -k -0.5
-run niblack sonnet sonnet20 -r 20 -k -0.5
-run niblack wedge wedge -r 50 -k -0.3
-run niblack fiducial fiducial -r 50 -k -0.5
+# run niblack sonnet sonnet50 -r 50 -k -0.5
+# run niblack sonnet sonnet20 -r 20 -k -0.5
+# run niblack wedge wedge -r 50 -k -0.3
+# run niblack fiducial fiducial -r 50 -k -0.5
+
+mkdir -p resultados/sauvola
+run sauvola sonnet sonnet -k 0.08 -R 100
+run sauvola retina retina -k -0.5 -R 100
+run sauvola wedge wedge -k 0.08 -R 200
+run sauvola monarch monarch -k 0.5 -R 300
