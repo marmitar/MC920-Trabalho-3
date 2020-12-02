2392116dd18ffa2bfa7657e9a4d83dac2f0f4c59
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-01 21:55:26 -0300

    Geração global

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+echo -n Histogramas:
 mkdir -p resultados/base
 mkdir -p resultados/hist
 for img in $(ls imagens)
 do
     nome="${img%.*}"
-    convert imagens/$img resultados/base/$nome.png
-    python3 histograma.py imagens/$img resultados/hist/$nome.pgf
+    echo -n ' ' $nome
+    # convert imagens/$img resultados/base/$nome.png
+    # python3 histograma.py imagens/$img resultados/hist/$nome.pgf
 done
+echo
+
+function run {
+    argv=($@)
+    python3 limiarizacao -o resultados/$1/$3.png \
+        imagens/$2.pgm $1 ${argv[@]:3}
+    echo $1 $3 $(python3 porcentagem.py resultados/$1/$3.png)
+}
+
+mkdir -p resultados/global
+run global retina retina -T 128
+run global wedge wedge100 -T 100
+run global wedge wedge110 -T 110
