641a5db5a5e19584774be01eb38eea4d5b85a9b0
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-02 04:20:23 -0300

    Método do Contraste

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ do
     # convert imagens/$img resultados/base/$nome.png
     # python3 histograma.py imagens/$img resultados/hist/$nome.pgf
 done
-echo
+echo; echo
 
 function run {
     argv=($@)
@@ -23,26 +23,36 @@ mkdir -p resultados/global
 # run global retina retina -T 128
 # run global wedge wedge100 -T 100
 # run global wedge wedge110 -T 110
+echo
 
 mkdir -p resultados/bernsen
 # run bernsen baboon baboon -r 50
 # run bernsen peppers peppers -r 50
 # run bernsen wedge wedge -r 20
 # run bernsen sonnet sonnet -r 20
+echo
 
 mkdir -p resultados/niblack
 # run niblack sonnet sonnet50 -r 50 -k -0.5
 # run niblack sonnet sonnet20 -r 20 -k -0.5
 # run niblack wedge wedge -r 50 -k -0.3
 # run niblack fiducial fiducial -r 50 -k -0.5
+echo
 
 mkdir -p resultados/sauvola
 # run sauvola sonnet sonnet -k 0.08 -R 100
 # run sauvola retina retina -k -0.5 -R 100
 # run sauvola wedge wedge -k 0.08 -R 200
 # run sauvola monarch monarch -k 0.5 -R 300
+echo
 
 mkdir -p resultados/phansalkar
-run phansalkar retina retina -r 5 -k -0.2 -q 0.5 -p 0.2 -R 4
-run phansalkar monarch monarch -r 10 -k 0.9 -q 10 -p 5 -R 0.7
-run phansalkar wedge wedge -r 10 -k 0.1 -p 12 -q 16 -R 0.9
+# run phansalkar retina retina -r 5 -k -0.2 -q 0.5 -p 0.2 -R 4
+# run phansalkar monarch monarch -r 10 -k 0.9 -q 10 -p 5 -R 0.7
+# run phansalkar wedge wedge -r 10 -k 0.1 -p 12 -q 16 -R 0.9
+# echo
+
+mkdir -p resultados/contraste
+run contraste baboon baboon -r 50
+run contraste sonnet sonnet -r 20
+echo
