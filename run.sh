30ab2e84ede5d997fe59b224b88aa05d5744155c
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-01 23:23:59 -0300

    Método de Bernsen

--- run.sh
+++ run.sh
@@ -19,7 +19,13 @@ function run {
     echo $1 $3 $(python3 porcentagem.py resultados/$1/$3.png)
 }
 
-mkdir -p resultados/global
-run global retina retina -T 128
-run global wedge wedge100 -T 100
-run global wedge wedge110 -T 110
+# mkdir -p resultados/global
+# run global retina retina -T 128
+# run global wedge wedge100 -T 100
+# run global wedge wedge110 -T 110
+
+mkdir -p resultados/bernsen
+run bernsen baboon baboon -r 50
+run bernsen peppers peppers -r 50
+run bernsen wedge wedge -r 20
+run bernsen sonnet sonnet -r 20
